a1d14413ea136c5f8113969ec0bb62a806d12bda
Polina Demchuk <polina.demchuk9@gmail.com>
2025-02-21 15:41:29 +0300

    add scripts dir

--- scripts/run_with_random_seed.sh
+++ scripts/run_with_random_seed.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-[[ $1 ]] || echo "need seed"
+usage() {
+    echo "Usage: $0 seed"
+    exit 1
+}
 
 seed=$1
+[[ $seed ]] || usage
 
 spin -p -s -r -X -v -n"$1" -l -g -u10000 autotune.pml > log
 spin -s -r -X -v -n"$1" -l -g -u10000 autotune.pml > log_full
